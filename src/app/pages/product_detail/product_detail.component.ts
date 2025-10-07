6e9b7a11299b254a890cd6882655faeb1646e3fb
Omar <omar.ramirezm65@gmail.com>
2025-10-07 16:20:03 -0600

    Clase 7 octubre

--- src/app/pages/product_detail/product_detail.component.ts
+++ src/app/pages/product_detail/product_detail.component.ts
@@ -1,36 +1,40 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FakeStoreService } from '../../services/fakeStore.service';
+import { ProductFake } from '../../app.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
-  template: `<p>product_detail works!</p>`,
-  styleUrl: './product_detail.component.css',
+  standalone: true, 
+  imports: [CommonModule],
+  templateUrl: './product_detail.component.html',
+  styleUrls: ['./product_detail.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
-
   private _activeRoute = inject(ActivatedRoute);
   private _apiService = inject(FakeStoreService);
 
-  constructor(){
+  producto!: ProductFake;
+
+  constructor() {
     this._activeRoute.params.subscribe({
-      next: (params)=> {
-        const {id} = params;
+      next: (params) => {
+        const { id } = params;
         this.getProduct(id);
       },
-      error: (error) => console.log(error)
-    })
-
+      error: (error) => console.error(error),
+    });
   }
 
-  getProduct(id: string){
+  getProduct(id: string) {
     this._apiService.getProduct(id).subscribe({
       next: (miProducto) => {
-        console.log(miProducto)
+        this.producto = miProducto;
+        console.log('Producto cargado:', this.producto);
       },
-      error: console.log
+      error: (error) => console.error('Error al obtener producto:', error),
     });
   }
 }
